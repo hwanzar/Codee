b33efba20e6beaac54884d49fd18950d153a4e87
Dang Hoang Gia <giadh1103@gmail.com>
2022-10-05 10:39:53 +0700

    new update

---
+++ CP/BFS-DFS/dfs_polandball.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using vi = vector<int>;
+#define pb push_back
+
+int N, M;
+vector<int> adj[N];
+bool visited[N];
+
+void dfs(int s)
+{
+    if (visited[s])
+        return;
+    visited[s] = true;
+    // process node s
+    for (auto u : adj[s])
+    {
+        dfs(u);
+    }
+}
+int main()
+{
+}
