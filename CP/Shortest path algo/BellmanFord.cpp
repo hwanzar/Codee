38cbf3a7be5b0dbc51ea95f4d51ba83a98691b9b
gia <giadh1103@gmail.com>
2022-10-19 13:11:30 +0700

    a daily commit

--- CP/Shortest path algo/BellmanFord.cpp
+++ CP/Shortest path algo/BellmanFord.cpp
@@ -1,18 +1,26 @@
-#include <bits/stdc++.h>
-using namespace std;
+int n; // Số lượng đỉnh của đồ thị
+int d[maxN], par[maxN];
+bool visit[maxN];
+vector <int> g[maxN];
 
+void bfs(int s) { // Với s là đỉnh xuất phát (đỉnh nguồn)
+    fill_n(d, n + 1, 0);
+    fill_n(par, n + 1, -1);
+    fill_n(visit, n + 1, false);
 
-
-for (int i = 1; i <= n; i++)
-    distance[i] = INF;
-distance[x] = 0;
-for (int i = 1; i <= n - 1; i++)
-{
-    for (auto e : edges)
-    {
-        int a, b, w;
-        tie(a, b, w) = e;
-        distance[b] = min(distance[b], distance[a] + w);
+    queue <int> q;
+    q.push(s);
+    visit[s] = true;
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        for (auto v : g[u]) {
+            if (!visit[v]) {
+                d[v]     = d[u] + 1;
+                par[v]   = u;
+                visit[v] = true;
+                q.push(v);
+            }
+        }
     }
 }
-for (int)
