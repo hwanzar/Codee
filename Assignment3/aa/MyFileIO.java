a959b98afa295aae3e6f2b84da61be40388e684a
gia <giadh1103@gmail.com>
2022-10-24 17:50:39 +0700

    a daily commit

---
+++ Assignment3/aa/MyFileIO.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+
+public interface MyFileIO {  
+    public void readData(Scanner input);
+	public void writeData(Formatter output);
+	public String toString();
+}
+
