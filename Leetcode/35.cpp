1c09a13b9d42bcc06a1cb295cdfa8225f362e58b
gia <giadh1103@gmail.com>
2022-11-09 18:48:59 +0700

    a daily commit

---
+++ Leetcode/35.cpp
@@ -0,0 +1,40 @@
+// Search index position:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        // cout << nums.size();
+        int ans = n;
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] >= target)
+            {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int>nums;
+    for(int i = 0; i < n;i++){
+	    int x; cin >> x;
+	    nums.push_back(x);
+    }
+    int tar;
+    cin >> tar;
+    Solution solve;
+    int res = solve.searchInsert(nums,tar);
+    cout << res << endl;
+    return 0;
+}
