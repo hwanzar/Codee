f66671df37fc359de242af62d218897c864839f9
hwanzar <giadh1103@gmail.com>
2023-01-29 11:17:24 +0700

    abs

---
+++ code.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void hello(){
+  cout << "Hello Hello" << endl;
+  return;
+}
+
+int helloo(){
+  return 11111111;
+}
+
+int main(){
+  hello();
+  cout << helloo();
+return 0;
+}
