95e8f52ac872f6277c00becab0543f7385469823
hwanzar <giadh1103@gmail.com>
2023-02-05 19:34:47 +0700

    daily commit

---
+++ Dojo/SLinkedList.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<class T>
+class IList{
+  public:
+    virtual ~IList(){};
+    virtual void add(T e)=0;
+    
+    virtual void add(int index,T e)=0;
+    
+};
+
+int main(){
+
+}
